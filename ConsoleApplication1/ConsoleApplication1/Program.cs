22847247e606f4d2fd324ab24d630a6e4f0f4ffa
MarcusIlitera <marcus.sp.net@ilitera.com>
2024-01-05 12:51:51 -0300

    Adicionando um arquivo e fazendo a soma de dois números

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 5;
+            int soma = a + b;
+            Console.Write(a + " + " + b + " = " + soma);
+            Console.Read();
+        }
+    }
+}
