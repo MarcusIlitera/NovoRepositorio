0a2ed5a1bdc6dfc551e4185775949640470107b6
MarcusIlitera <marcus.sp.net@ilitera.com>
2024-01-08 11:51:21 -0300

    fa

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApplication1
             int b = 5;
             int soma = a + b;
             Console.Write(a + " + " + b + " = " + soma);
+            tssts
             Console.Read();
         }
     }
